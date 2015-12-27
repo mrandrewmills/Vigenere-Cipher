bc7ef0e07fe7ba73579d4f02561c096b2592c629
Andrew Mills <andrewmills@knowwwhow.com>
2015-12-27 15:12:41 -0500

    clean up, JSLint
    tidied up formatting, now passes JSLint default checks.

--- vigenere.js
+++ vigenere.js
@@ -1,95 +1,148 @@
-// create our object
-var vigenere = {};
+/**
+ *  vigenere.js - JavaScript Object for Vigenere ciphers
+ *  @type {object}
+ *  @namespace
+ */
+var vigenere = {
 
-// create our properties
-vigenere.plaintext = "";
-vigenere.ciphertext = "";
-vigenere.keyword = "";
+      /**
+       * plaintext - the real, original message
+       * @type {String}
+       */
+        plaintext: "",
 
-// now let's create our Vigenere table
-vigenere.alphabets = [];
-vigenere.alphabets[0] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-for (x = 1; x < vigenere.alphabets[0].length; x++) {
-    vigenere.alphabets[x] = vigenere.alphabets[0].substr(x);
-    vigenere.alphabets[x] = vigenere.alphabets[x].concat(vigenere.alphabets[0].substring(0,x));
-}
+      /**
+       * ciphertext - the encrypted version of the message
+       * @type {String}
+       */
+        ciphertext: "",
 
-vigenere.encrypt = function encrypt(plaintext, password) {
+      /**
+       * keyword - the password used to encrypt and decrypt the ciphertext
+       * @type {String}
+       */
+        keyword: "",
 
-	// initialize defaults
-	this.keyword = password.toUpperCase();
-	this.plaintext = plaintext.toUpperCase();
-	this.ciphertext = "";
+      /**
+       * alphabets - a slightly modified version of the Vigenere table,
+       * @type {Array}
+       */
+        alphabets: [],
 
-	// we'll need to keep track of which letter of the password we're currently using
-	var pwIndex = 0;
+        /**
+         *
+         */
+        init: function init() {
 
-	// time to traverse the plaintext message
-     for (var x = 0; x < this.plaintext.length; x++) {
+            "use strict";
 
-		// first figure out which row of the Vigenere table we must use
-		vRow = this.alphabets[0].indexOf(this.keyword[pwIndex]);
-		thisLetter = this.alphabets[0].indexOf(this.plaintext[x]);
+            var x;
 
-		// if the next char of plaintext is NOT a capital letter
-		if (thisLetter === -1) {
-			// then just pass it through unchanged
-			this.ciphertext += this.plaintext[x];
-		} else {
-			// otherwise find its counterpart in the V. table
-			thisRow = this.alphabets[vRow];
-			this.ciphertext += thisRow[thisLetter];
-		}
+            // populate the first row of our Vigenere table
+            this.alphabets[0] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
-		// don't forget to move to next letter in our password
-		pwIndex++;
+            // populate the rest of our Vigenere table
+            for (x = 1; x < this.alphabets[0].length; x = x + 1) {
+                this.alphabets[x] = this.alphabets[0].substr(x);
+                this.alphabets[x] = this.alphabets[x].concat(this.alphabets[0].substring(0, x));
+            }
+        },
 
-		// if we "run out of password", start back at 1st letter again
-		if (pwIndex >= this.keyword.length) {
-			pwIndex = 0; 
-		}
-	}
+        /**
+         *
+         */
+        encrypt : function encrypt(plaintext, password) {
 
-	// now we're ready to share the encrypted result
-	return this.ciphertext;
-}
+            "use strict";
 
-vigenere.decrypt = function decrypt(ciphertext, password) {
+            var x, pwIndex, vRow, thisLetter, thisRow;
 
-	// initialize defaults
-	this.keyword = password.toUpperCase();
-	this.plaintext = "";
-	this.ciphertext = ciphertext;
+            // initialize defaults
+            this.keyword = password.toUpperCase();
+            this.plaintext = plaintext.toUpperCase();
+            this.ciphertext = "";
 
-	// we'll need to keep track of which letter of the password we're currently using
-	var pwIndex = 0;
+            // we'll need to keep track of which letter of the password we're currently using
+            pwIndex = 0;
 
-	// let's traverse the ciphertext message
-     for (var x = 0; x < this.ciphertext.length; x++) {
+            // time to traverse the plaintext message
+            for (x = 0; x < this.plaintext.length; x = x + 1) {
+                // first figure out which row of the Vigenere table we must use
+                vRow = this.alphabets[0].indexOf(this.keyword[pwIndex]);
+                thisLetter = this.alphabets[0].indexOf(this.plaintext[x]);
 
-		// figure out which row of the Vigenere table should have been used with this password
-		vRow = this.alphabets[0].indexOf(this.keyword[pwIndex]);
-		thisLetter = this.alphabets[vRow].indexOf(this.ciphertext[x]);
+                // if the next char of plaintext is NOT a capital letter
+                if (thisLetter === -1) {
+                    // then just pass it through unchanged
+                    this.ciphertext += this.plaintext[x];
+                } else {
+                    // otherwise find its counterpart in the V. table
+                    thisRow = this.alphabets[vRow];
+                    this.ciphertext += thisRow[thisLetter];
+                }
 
-		// if the next char of ciphertext is NOT a capital letter
-		if (thisLetter === -1) {
-			// then just pass it through unchanged
-			this.plaintext += this.ciphertext[x];
-		} else {
-			// otherwise find its counterpart "backwards" in the V. table
-			thisRow = this.alphabets[0]; // this will always be the first row
-			this.plaintext += thisRow[thisLetter];
-		}
+                // don't forget to move to next letter in our password
+                pwIndex = pwIndex + 1;
 
-		// don't forget to move to next letter in our password
-		pwIndex++;
+                // if we "run out of password", start back at 1st letter again
+                if (pwIndex >= this.keyword.length) {
+                    pwIndex = 0;
+                }
+            }
 
-		// if we "run out of password", start back at 1st letter again
-		if (pwIndex >= this.keyword.length) {
-			pwIndex = 0; 
-		}
-	}
+            // now we're ready to share the encrypted result
+            return this.ciphertext;
+        },
 
-	// now we're ready to share the decrypted result 
-	return this.plaintext;
-}
+        /**
+         *
+         */
+        decrypt : function decrypt(ciphertext, password) {
+
+            "use strict";
+
+            // initialize defaults
+            this.keyword = password.toUpperCase();
+            this.plaintext = "";
+            this.ciphertext = ciphertext;
+
+            // we'll need to keep track of which letter of the password we're currently using
+            var pwIndex, x, vRow, thisLetter, thisRow;
+
+            // we'll need to keep track of which letter of the password we're currently using
+            pwIndex = 0;
+
+            // let's traverse the ciphertext message
+            for (x = 0; x < this.ciphertext.length; x = x + 1) {
+
+                // figure out which row of the Vigenere table should have been used with this password
+                vRow = this.alphabets[0].indexOf(this.keyword[pwIndex]);
+                thisLetter = this.alphabets[vRow].indexOf(this.ciphertext[x]);
+
+                // if the next char of ciphertext is NOT a capital letter
+                if (thisLetter === -1) {
+                    // then just pass it through unchanged
+                    this.plaintext += this.ciphertext[x];
+                } else {
+                    // otherwise find its counterpart "backwards" in the V. table
+                    thisRow = this.alphabets[0]; // this will always be the first row
+                    this.plaintext += thisRow[thisLetter];
+                }
+
+                // don't forget to move to next letter in our password
+                pwIndex = pwIndex + 1;
+
+                // if we "run out of password", start back at 1st letter again
+                if (pwIndex >= this.keyword.length) {
+                    pwIndex = 0;
+                }
+            }
+
+            // now we're ready to share the decrypted result
+            return this.plaintext;
+        }
+
+    };
+
+// to build out our Vigenere table
+vigenere.init();
